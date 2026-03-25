27a386ab13d38a47c6a987baede7ce3175f1eebe
Andrew <mcdrew1478@gmail.com>
2026-03-24 18:37:28 -0600

    Updated movement and created asteroids

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
-from logger import log_state
+from asteroid import Asteroid
+from player import Player
+from logger import log_state, log_event
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -12,6 +15,15 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # Create the player at the center of the screen
+    AsteroidField() # Create the asteroid field
     # Main game loop
     while True:
         log_state()
@@ -19,6 +31,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(("black"))
+        updatable.update(dt) # Update all sprites in the updatable group, passing the delta time
+        for drawing in drawable: # Draw all sprites in the drawable group
+            drawing.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000  # Cap the frame rate at 60 FPS and store delta time in seconds
 
